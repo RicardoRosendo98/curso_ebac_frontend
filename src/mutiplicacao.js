b23351c23be95bfbfa2e70ae938c8dfabf12947f
Ricardo Rosendo <88691117+RicardoRosendo98@users.noreply.github.com>
2023-06-04 16:57:22 -0300

    Exercicio_ts

---
+++ src/mutiplicacao.js
@@ -0,0 +1,13 @@
+"use strict";
+// Mutiplicação
+function mutiplicacaoNumeros(xNumero, yNumero) {
+    return xNumero * yNumero;
+}
+const resultadoMultiplicacao = mutiplicacaoNumeros(28, 56);
+console.log(resultadoMultiplicacao);
+// Saudação
+function saudacao(nome) {
+    return "ola " + nome;
+}
+const saudacaoMenssagem = saudacao("Ricardo");
+console.log(saudacaoMenssagem);
---
+++ src/mutiplicacao.ts
@@ -0,0 +1,21 @@
+// Mutiplicação
+function mutiplicacaoNumeros(xNumero: number, yNumero: number): number {
+
+    return xNumero * yNumero;
+
+}
+
+const resultadoMultiplicacao = mutiplicacaoNumeros(28, 56);
+console.log(resultadoMultiplicacao);
+
+
+// Saudação
+function saudacao(nome: string): string {
+    return "ola " + nome;
+}
+
+const saudacaoMenssagem = saudacao ("Ricardo");
+console.log(saudacaoMenssagem);
+
+
+
